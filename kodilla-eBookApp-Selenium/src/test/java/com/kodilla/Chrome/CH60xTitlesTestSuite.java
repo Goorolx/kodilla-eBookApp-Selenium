bf8e4434bfab4230c4b4ee705c819e81a9c21bde
Piotr Sutor <60980702+Goorolx@users.noreply.github.com>
2020-08-28 17:56:27 +0200

    Test suite 602 is checking now if it's possible to add title when empty fields

--- kodilla-eBookApp-Selenium/src/test/java/com/kodilla/Chrome/CH60xTitlesTestSuite.java
+++ kodilla-eBookApp-Selenium/src/test/java/com/kodilla/Chrome/CH60xTitlesTestSuite.java
@@ -25,7 +25,7 @@ public class CH60xTitlesTestSuite {
 
 
     @Test
-    public void t501shouldAdd2NewTitle(ChromeDriver driver) {
+    public void t601shouldAdd2NewTitle(ChromeDriver driver) {
         //given
         WebDriverWait wait = new WebDriverWait(driver, 5);
         driver.get("https://ta-ebookrental-fe.herokuapp.com/");
@@ -33,9 +33,9 @@ public class CH60xTitlesTestSuite {
 
         //log in to system, I should get to titles page
         WebElement log = driver.findElement(By.id("login"));
-        log.sendKeys("goorolx");
+        log.sendKeys(logingx);
         WebElement pwd = driver.findElement(By.name("password"));
-        pwd.sendKeys("rudy103");
+        pwd.sendKeys(pwdgx);
         WebElement button = driver.findElement(By.id("login-btn"));
         button.click();
 
@@ -71,18 +71,91 @@ public class CH60xTitlesTestSuite {
         List<WebElement> listAuthor = driver.findElementsByXPath("//li[contains(@id,'title-')]/div/div[2]");
         List<WebElement> listYear = driver.findElementsByXPath("//li[contains(@id,'title-')]/div/div[3]");
         //then
-        Assertions.assertEquals(4,listTitles.size()); // List should contain 2 elements
+        Assertions.assertEquals(4, listTitles.size()); // List should contain 2 elements
 
         for (int i = 0; i < listTitles.size(); i++)   // checking if titles are added correctly
-            if(i%2==0) Assertions.assertTrue((title).equalsIgnoreCase(listTitles.get(i).getText()));
-            else Assertions.assertTrue((title+2).equalsIgnoreCase((listTitles.get(i).getText())));
+            if (i % 2 == 0) Assertions.assertTrue((title).equalsIgnoreCase(listTitles.get(i).getText()));
+            else Assertions.assertTrue((title + 2).equalsIgnoreCase((listTitles.get(i).getText())));
 
-        for (int i=0;i<listAuthor.size();i++) { // checking if author is corect
-           Assertions.assertTrue(("by "+author).equalsIgnoreCase(listAuthor.get(i).getText()));}
+        for (int i = 0; i < listAuthor.size(); i++)  // checking if author is correct
+            Assertions.assertTrue(("by " + author).equalsIgnoreCase(listAuthor.get(i).getText()));
 
-        for (int i=0;i<listYear.size();i++)  //Checking if year is correct
-            if(i%2==0) Assertions.assertEquals(("("+year+")"),listYear.get(i).getText());
-            else Assertions.assertEquals(("("+newYear+")"),listYear.get(i).getText());
+        for (int i = 0; i < listYear.size(); i++)  //Checking if year is correct
+            if (i % 2 == 0) Assertions.assertEquals(("(" + year + ")"), listYear.get(i).getText());
+            else Assertions.assertEquals(("(" + newYear + ")"), listYear.get(i).getText());
+    }
 
+    @Test
+    public void t602shouldNotAddTitleWithEmptyFields(ChromeDriver driver) {
+        //given
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        driver.get("https://ta-ebookrental-fe.herokuapp.com/");
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-btn")));
+
+        //log in to system, I should get to titles page
+        WebElement log = driver.findElement(By.id("login"));
+        log.sendKeys(logingx);
+        WebElement pwd = driver.findElement(By.name("password"));
+        pwd.sendKeys(pwdgx);
+        WebElement button = driver.findElement(By.id("login-btn"));
+        button.click();
+
+        //when
+        //Adding new Title
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); //Added Implicit wait because Explicit wait below not always worked
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("titles")));
+        //get titles to compare
+        List<WebElement> listTitlesBefore = driver.findElementsByXPath("//li[contains(@id,'title-')]/div/div[1]");
+
+        WebElement addTitle = driver.findElement(By.cssSelector("#add-title-button"));
+        addTitle.click();
+        //602a - Try Add Title with empty Title
+        driver.findElement(By.xpath("//input[@name='title']")).sendKeys("");
+        driver.findElement(By.xpath("//input[@name='author']")).sendKeys(author);
+        driver.findElement(By.xpath("//input[@name='year']")).sendKeys(year.toString());
+
+        driver.findElement(By.xpath("//*[@name='submit-button']")).click();
+
+        Assertions.assertTrue(driver.findElementByCssSelector("[class*=alert--error]").isDisplayed());
+        List<WebElement> listTitleAfter = driver.findElementsByXPath("//li[contains(@id,'title-')]/div/div[1]");
+        Assertions.assertEquals(listTitlesBefore, listTitleAfter); //Comparing lists, checking if Title is added
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class*=icon-close]"))).click();
+
+        //602b - Try Add Title with empty Author
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); //Added Implicit wait because Explicit wait below not always worked
+
+        //We need list of authors before and after
+        List<WebElement> listAuthorBefore = driver.findElementsByXPath("//li[contains(@id,'title-')]/div/div[2]");
+
+        driver.findElement(By.cssSelector("#add-title-button")).click();
+        driver.findElement(By.xpath("//input[@name='title']")).sendKeys(title);
+        driver.findElement(By.xpath("//input[@name='author']")).sendKeys("");
+        driver.findElement(By.xpath("//input[@name='year']")).sendKeys(year.toString());
+
+        driver.findElement(By.xpath("//*[@name='submit-button']")).click();
+
+        Assertions.assertTrue(driver.findElementByCssSelector("[class*=alert--error]").isDisplayed());
+        List<WebElement> listAuthorAfter = driver.findElementsByXPath("//li[contains(@id,'title-')]/div/div[2]");
+        Assertions.assertEquals(listAuthorBefore, listAuthorAfter); //Comparing lists, checking if title is added
+
+        //Now third one for Year
+        //602c - Try Add Title with empty year
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("[class*=icon-close]"))).click();
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS); //Added Implicit wait because Explicit wait below not always worked
+
+        //We need list of Years before and after
+        List<WebElement> listYearBefore = driver.findElementsByXPath("//li[contains(@id,'title-')]/div/div[3]");
+
+        driver.findElement(By.cssSelector("#add-title-button")).click();
+        driver.findElement(By.xpath("//input[@name='title']")).sendKeys(title);
+        driver.findElement(By.xpath("//input[@name='author']")).sendKeys("");
+        driver.findElement(By.xpath("//input[@name='year']")).sendKeys("");
+
+        driver.findElement(By.xpath("//*[@name='submit-button']")).click();
+
+        Assertions.assertTrue(driver.findElementByCssSelector("[class*=alert--error]").isDisplayed());
+        List<WebElement> listYearAfter = driver.findElementsByXPath("//li[contains(@id,'title-')]/div/div[3]");
+        Assertions.assertEquals(listYearBefore, listYearAfter); //Comparing lists, checking if title is added
     }
 }
