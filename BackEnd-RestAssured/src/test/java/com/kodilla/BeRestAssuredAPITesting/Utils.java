15ee9e076485036c7020cc3882407577b63462ed
Piotr Sutor <60980702+Goorolx@users.noreply.github.com>
2020-09-29 09:43:44 +0200

    Added 2 new tests, moved Json Object creation to Utils class

--- BackEnd-RestAssured/src/test/java/com/kodilla/BeRestAssuredAPITesting/Utils.java
+++ BackEnd-RestAssured/src/test/java/com/kodilla/BeRestAssuredAPITesting/Utils.java
@@ -1,2 +1,30 @@
-package com.kodilla.BeRestAssuredAPITesting;public class Utils {
+package com.kodilla.BeRestAssuredAPITesting;
+
+import org.json.JSONObject;
+import org.testng.annotations.DataProvider;
+
+public final class Utils {
+    public static final String baseUrl = "https://ta-ebookrental-be.herokuapp.com";
+    public static final String registerUrl = "https://ta-ebookrental-be.herokuapp.com/user/register";
+    public static final String loginUrl = "https://ta-ebookrental-be.herokuapp.com/user/login";
+
+    public static final String testUser = "John Tester";
+    public static final String testUsrPwd = "rudy1031";
+
+    @DataProvider(name="loginDataFail")
+    public static Object [][] createTestData(){
+        return new Object [][]{
+                {"Zbigniew","kolo201"},
+        {"CZeslaww","dzoi00990"},
+        };
+
+    }
+
+    public static JSONObject jsonObjectUserBuilder(String login, String pwd){
+        JSONObject json = new JSONObject()
+                .put("login",login)
+                .put("password", pwd);
+        return json;
+    }
+
 }
--- BackEnd-RestAssured/src/test/java/com/kodilla/BeRestAssuredAPITesting/t10xUserRestAssuredTestSuite.java
+++ BackEnd-RestAssured/src/test/java/com/kodilla/BeRestAssuredAPITesting/t10xUserRestAssuredTestSuite.java
@@ -1,2 +1,57 @@
+package com.kodilla.BeRestAssuredAPITesting;
+
+
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import static com.kodilla.BeRestAssuredAPITesting.Utils.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+
 public class t10xUserRestAssuredTestSuite {
+    private int usrId ;
+
+    @Test
+    public void t101_givenUrl_whenSuccessRegistration_thenResponseHasIdAndNewEqualsTrue() {
+        JSONObject json = new JSONObject()   //building Json with registration data
+                .put("login", "John Tester")
+                .put("password", "rudy1031");
+        given().with()
+                .contentType("application/json")  //another way to specify content type
+                .body(json.toString())
+                .when().post(registerUrl)
+                .then().statusCode(200)
+                .assertThat().body("id", greaterThan(210))  //last manually added user has id of 210+
+                .assertThat().body("new", is(true));  //when user already exists new is false
+    }
+
+    @Test
+    public void givenLoginUrl_whenSuccessLogin_thenResponseUserId(){
+        JSONObject json = jsonObjectUserBuilder(testUser,testUsrPwd);
+        given().with()
+                .contentType("application/json")  //another way to specify content type
+                .body(json.toString())
+                .when().post(loginUrl)
+                .then().statusCode(200)
+                .assertThat().body( is("210"));
+    }
+
+
+
+    @Test
+    public void givenUrl_whenSuccessOnGetsResponseAndJsonHasAuthor_thenCorrect() {
+        given()
+                .when().get(baseUrl + "/titles/?userId=210")
+                .then().statusCode(200)
+                .assertThat().body("author", hasItems("John Doe"));
+    }
+
+    @Test
+    public void getRequest() {
+        given()
+                .when().get(baseUrl + "/titles/?userId=210")
+                .then().assertThat().statusCode(200);
+
+    }
 }
