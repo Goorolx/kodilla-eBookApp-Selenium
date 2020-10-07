6fb35f1f46d181b352090ec1d224eaddd8c08a3c
Piotr Sutor <60980702+Goorolx@users.noreply.github.com>
2020-10-07 15:39:04 +0200

    add t205 - delete title. Updated t201, now it saves newly added title ID so it can be used in further tests

--- BackEnd-RestAssured/src/test/java/com/kodilla/BeRestAssuredAPITesting/t20xTitleRestAssuredTestSuite.java
+++ BackEnd-RestAssured/src/test/java/com/kodilla/BeRestAssuredAPITesting/t20xTitleRestAssuredTestSuite.java
@@ -1,6 +1,8 @@
 package com.kodilla.BeRestAssuredAPITesting;
 
+import io.restassured.response.Response;
 import org.json.JSONObject;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static com.kodilla.BeRestAssuredAPITesting.Utils.*;
@@ -8,34 +10,55 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
 
 public class t20xTitleRestAssuredTestSuite {
+    String testTitle;
+    String testAuthor;
+    int testYear;
+    int testTitleId;
 
-    @Test
+    @Test(priority = 0)
     public void t201_givenTitleUrl_whenPostNewTitle_thenResponseHasTitleId() {
         //given
-        String jsonString = jsonStringTitleBuilder(210, "War and Peace", "Lew Tołstoj", 1869);
+        testTitle = "War and Peace";
+        testAuthor = "Lew Tołstoj";
+        int testYear = 1869;
+        int expStatus = 200;
+        int oldTestId = 1020; //Id 1020 was returned when tested manually
+        //building Json object
+        String jsonString = jsonStringTitleBuilder(testUsrId, testTitle, testAuthor, testYear);
 
         //when
-        given()
+        Response response = given()
                 .contentType("application/json")
                 .body(jsonString)
                 .when()
                 .post(titleUrl)
                 //then
-                .then().statusCode(200)
-                .assertThat().body(greaterThanOrEqualTo(405));  //Id 405 was returned when tested manually
+                .then().statusCode(expStatus)
+                .extract().response();
+
+//passing titleId as integer for assertion and following tests
+        testTitleId = Integer.parseInt(response.getBody().asString());
+        Assert.assertTrue(testTitleId > oldTestId);
+        System.out.println(testTitleId);
     }
 
     @Test
     public void t202_givenTitleUrl_whenGetTitlesList_thenResponseHasTitleFromt201() {
         //given
-        given().queryParam("userId", "210")
+        testTitle = "War and Peace";
+        testAuthor = "Lew Tołstoj";
+        int testYear = 1869;
 
-                //when + then
+        //when + then
+        given().queryParam("userId", testUsrId)
                 .when()
                 .get(titleUrl)
                 .then().statusCode(200)
-                .assertThat().body(hasItems("War and Peace", "Lew Tołstoj", "1869"));
-//TODO fix assertion above
+                .assertThat()
+                .body("id", hasItem(testTitleId))  //created in t201
+                .body("title", hasItem(testTitle))
+                .body("author", hasItem(testAuthor))
+                .body("year", hasItem(testYear));
     }
 
     @Test
@@ -74,13 +97,13 @@ public class t20xTitleRestAssuredTestSuite {
         //TODO think about this one, how to force missing year? possible?
     }
 
-    @Test
-    public void t204_givenTitleUrl_whenPutTitle_thenEditedSuccess(){
+    @Test(priority = 1)
+    public void t204_givenTitleUrl_whenPutTitle_thenEditedSuccess() {
         //given
         String testTitle = "Kalarepa";
         String testAuthor = "Ann Marchew";
         int testYear = 1008;
-        int titleId = 406;
+        int titleId = testTitleId;
         JSONObject json = new JSONObject()
                 .put("userId", testUsrId)
                 .put("id", titleId)
@@ -93,14 +116,39 @@ public class t20xTitleRestAssuredTestSuite {
         given().contentType("application/json")
                 .body(json.toString())
                 .when().put(titleUrl)
-        //then
+                //then
                 .then().statusCode(expStatus)
                 .assertThat()
                 .body("id", is(titleId))
                 .body("title", is(testTitle))
                 .body("author", is(testAuthor))
                 .body("year", is(testYear));
+    }
 
+    @Test
+    public void t205_givenTitleUrl_whenDeleteTitle_thenResponseTrue() {
+        //given
+        String expResponse = "true";
+        //int titleIdToRemove = testTitleId;
+        int titleIdToRemove = 1022;
+        int expStatus = 200;
+
+        //when
+        given().queryParam("userId", testUsrId)
+                .queryParam("id", titleIdToRemove)
+                .when().delete(titleUrl)
+                //then
+                .then().statusCode(expStatus)
+                .assertThat().body(is(expResponse));
+
+        //when + then   //part of t202
+        given().queryParam("userId", testUsrId)
+                .when()
+                .get(titleUrl)
+                .then().statusCode(200)
+                .assertThat()
+                .body("id", not(titleIdToRemove));
+       // TODO this assertion is always true, I have to think about it
     }
 
 
